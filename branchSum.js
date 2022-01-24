907de535d0ecbe1eee399e3f57a5dcf8b1501a7d
FrankBatista1 <fb53901@gmail.com>
2022-01-23 23:49:30 -0500

    branch sum algorithm o(n) ts

---
+++ branchSum.js
@@ -0,0 +1,28 @@
+// O(n) T | O(n) S 
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value; 
+    this.left = null; 
+    this.right = null;
+  }
+}
+
+function calculateBranchSums(node, runningSum, sums){
+  if (!node) return;
+  const newRunningSum = runningSum + node.value;
+  if(!node.left && !node.right){
+    sums.push(newRunningSum);
+    return;
+  }
+
+  calculateBranchSums(node.left, newRunningSum, sums)
+  calculateBranchSums(node.right, newRunningSum, sums)
+}
+
+function branchSums (root) {
+  const sums = [];
+  calculateBranchSums(root, 0, sums );
+  return sums;
+}
+
