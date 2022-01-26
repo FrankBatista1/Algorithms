4a3fcaedb42b0f12fc03ed9e7f6a0d3d46cbb2c6
FrankBatista1 <fb53901@gmail.com>
2022-01-25 23:10:03 -0500

    validatesubsequence

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -2,9 +2,10 @@
 
 const validateSubsequence = (array, sequence) => {
   let seqIdx = 0; 
-  for (const nums of array ){
+  for (const value of array ){
     if(seqIdx === sequence.length) break; 
     if(sequence[seqIdx] === value) seqIdx++;
   }
+  //This will return true if is a sequence and false if it's not
   return seqIdx === sequence.length
 }
