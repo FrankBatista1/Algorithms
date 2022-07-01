1979d393a937d4f7792d59fe254d83eab1bb60ef
FrankBatista1 <fb53901@gmail.com>
2022-06-30 23:41:09 -0400

    fib quest

--- threeNumberSum.js
+++ threeNumberSum.js
@@ -1,5 +1,4 @@
 function threeNumberSum(array, targetSum) {
-<<<<<<< HEAD
   array.sort((a, b) => a - b);
   const triplets = [];
   for (let i = 0; i < array.length - 2; i++){
@@ -20,25 +19,4 @@ function threeNumberSum(array, targetSum) {
   }
   return triplets;
 }
-=======
-    array.sort((a, b) => a - b);
-    const triplets = [];
-    for (let i = 0; i < array.length - 2; i++){
-      let left = i + 1;
-      let right = array.length - 1;
-      while (left < right){
-        const sum = array[i] + array[left] + array[right];
-        if (sum === targetSum) {
-          triplets.push([array[i], array[left], array[right]])
-          left++;
-          right--;
-        } else if (sum > targetSum) {
-          right--;
-        } else if (sum < targetSum) {
-          left++;
-        }
-      }
-    }
-    return triplets;
-  }
->>>>>>> ecdceb90f9a1499b5f15322191a954b5d0b775f9
+
