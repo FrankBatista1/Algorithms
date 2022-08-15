facc9331d7fa343732fa3e40d133dcfeecd8793d
FrankBatista1 <fb53901@gmail.com>
2022-08-14 23:44:07 -0500

    smallest difference algorithm

---
+++ smallestDifference.js
@@ -0,0 +1,27 @@
+function smallestDifference(arrayOne, arrayTwo) {
+  arrayTwo.sort((a,b) => a - b);
+  arrayOne.sort((a,b) => a - b);
+  let idxOne = 0;
+  let idxTwo = 0;
+  let smallestDifference = Infinity; 
+  let currentDifference = Infinity;
+  let smallestPair = [];
+  while(idxOne < arrayOne.length && idxTwo < arrayTwo.length){
+    let firstNum = arrayOne[idxOne];
+    let secondNum = arrayTwo[idxTwo];
+    if (secondNum < firstNum){
+      currentDifference = firstNum - secondNum;
+      idxTwo++;
+    } else if (firstNum < secondNum){
+      currentDiffernece = secondNum - firstNum;
+      idxOne++;
+    } else {
+      return [firstNum, secondNum]; 
+    }
+    if (smallestDifference > currentDifference){
+      smallestDifference = currentDifference;
+      smallestPair = [firstNum, secondNum];
+    }
+  }
+  return smallestPair;
+}
