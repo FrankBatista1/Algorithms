67d155d2c22e1d015bfb68b7cee05c9bd25ea559
FrankBatista1 <fb53901@gmail.com>
2022-09-04 23:51:21 -0500

    better space complexity

--- generateDocument.js
+++ generateDocument.js
@@ -10,3 +10,22 @@ function generateDocument(characters, document) {
   }
   return true;
 }
+
+// O(1) Space
+
+function generateDocument(characters, document) {
+  for (const character of document) {
+    const documentFrequency = countCharacterFrequency(character, document);
+    const characterFrequency = countCharacterFrequency(character, characters);
+    if (documentFrequency > characterFrequency) return false;
+  }
+  return true;
+}
+
+function countCharacterFrequency(character, target) {
+  let frequency = 0; 
+  for (const char of target) {
+    if (char === character) frequency++;
+  }
+  return frequency;
+}
